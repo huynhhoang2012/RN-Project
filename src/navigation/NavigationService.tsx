08db4345d9d15145289af7593d2019047a62ef10
Hoang Huynh <hoanghuynh@Hunh.local>
2023-08-31 15:36:19 +0700

    remove log

--- src/navigation/NavigationService.tsx
+++ src/navigation/NavigationService.tsx
@@ -7,7 +7,6 @@ export function goBack() {
 }
 
 export function navigate(name: string, params?: Record<string, any>) {
-  console.log(navigationRef.isReady());
   if (navigationRef.isReady()) {
     navigationRef.navigate(name, params);
   }

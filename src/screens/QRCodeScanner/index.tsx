5251f20e8471992e80c3a4de4dcd809e4e2adf6d
Hoang Huynh <hoanghuynh@Hunh.local>
2023-09-05 16:40:38 +0700

    add lib react-native-permissions

---
+++ src/screens/QRCodeScanner/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+const QRCodeScanner = () => {
+  return (
+    <View style={styles.container}>
+      <Text>QRCodeScanner</Text>
+    </View>
+  );
+};
+
+export default QRCodeScanner;
--- src/screens/Setting/styles.ts
+++ src/screens/Setting/styles.ts
@@ -1,7 +1,38 @@
 import {StyleSheet} from 'react-native';
+import {WHITE} from '../../assets/colors';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+  },
+  viewFlatList: {
+    flex: 1,
+    paddingHorizontal: 8,
+  },
+  buttonSetting: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: WHITE,
+    borderRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  viewLeftItem: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  titleItem: {
+    marginLeft: 10,
+    fontSize: 14,
   },
 });
